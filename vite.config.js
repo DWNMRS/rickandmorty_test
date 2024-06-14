5537403deca2893ce07bee672799ea5191dd9f2c
DWNMRS <Downmars@yandex.ru>
2024-06-14 14:05:02 +0400

    work on gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import { resolve } from "node:path";
 
 export default defineConfig({
-  base: '/rickandmorty',
+  base: '/rickandmorty_test',
   plugins: [vue()],
   resolve: {
     alias: 
